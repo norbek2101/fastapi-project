d584598d2f30b45a6462287434898a88eeae36e9
norbek <lightnight1223@gmail.com>
2023-09-25 16:43:36 +0500

    change fields

--- models.py
+++ models.py
@@ -7,6 +7,4 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
-    first_name = Column(String)
-    last_name = Column(String)
     hashed_password = Column(String)
