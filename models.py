3f1a200f0d49726bbbd6d61be81c3d9c9d4e813b
norbek <lightnight1223@gmail.com>
2023-09-25 15:10:49 +0500

    user model was created

---
+++ models.py
@@ -0,0 +1,12 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
